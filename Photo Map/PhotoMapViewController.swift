43769bb1be040b5a2cbda75736dc1021b6fd48d2
Chris Lin <pwnzerness@gmail.com>
2018-02-22 23:27:24 -0500

    added permissions for camera

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
 class PhotoMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                              MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +25,9 @@ class PhotoMapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func onPhoto(_ sender: Any) {
+    }
+    
     
     // MARK: - Navigation
 
